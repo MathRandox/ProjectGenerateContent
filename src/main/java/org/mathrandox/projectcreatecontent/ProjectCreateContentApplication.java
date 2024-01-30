94c577b7ea208ff1b86d15430c177817c450036e
MathRandox <rmiesesc18_2@unc.edu.pe>
2024-01-30 16:32:02 -0500

    First commit

---
+++ src/main/java/org/mathrandox/projectcreatecontent/ProjectCreateContentApplication.java
@@ -0,0 +1,13 @@
+package org.mathrandox.projectcreatecontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCreateContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCreateContentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mathrandox/projectcreatecontent/ProjectCreateContentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mathrandox.projectcreatecontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCreateContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
